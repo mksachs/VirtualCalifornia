37991ad34cea3344e1ae86b7924427cb70e2bca0
mksachs <mksachs@ucdavis.edu>
2013-07-01 17:45:31 -0700

    changes for kasey

--- trunk/vcal_py/eli_demo.py
+++ trunk/vcal_py/eli_demo.py
@@ -12,11 +12,11 @@ import cPickle
 import h5py
 
 # should install by doing make on new_vcal
-import quakelib
+#import quakelib
 
 
 ''' testing webgui output '''
-data_file = '/Users/eliwoods/Documents/geosci/test/ALLCAL2_no-creep_dt-08_st-10.h5'
+data_file = '/Users/sachs/Documents/VirtualCalifornia/trunk/vcal_webgui/media/vc_data/ALLCAL2_no-creep_dt-08_st-10.h5'
 
 os.chdir(os.path.dirname(data_file))
 
@@ -26,13 +26,13 @@ tmp_sys_name = data_file_split[0]
 end_index = len(tmp_sys_name)
 sys_name = tmp_sys_name[0:end_index]
 
-vc_sys = cPickle.load(open('%s_cache/%s.pkl'%(sys_name,sys_name), 'rb'))
+vc_sys = VC.VCSys(sys_name, data_file)
 
 #event_detail = vc_sys.webgui_getEventDetail(data_file, 156)
 
 #print event_detail
 
-event_list = vc_sys.webgui_getEventDetail(data_file, 110112)
+event_list = vc_sys.webgui_getEventList(0, 1000)
 
 print event_list
 
