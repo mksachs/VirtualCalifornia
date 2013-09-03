d55a6f92c347092dcaa0bb1c1f1d4a20a0562447
Michael Sachs <mksachs@ucdavis.edu>
2013-09-03 16:06:56 -0700

    moving pyvc and webvc to their own repositories

--- pyvc/pyvc/__init__.py
+++
@@ -1,6 +0,0 @@
-from vcsys import VCSys
-from vcgeometry import VCGeometry
-from vcevents import VCEvents
-from vcutils import *
-
-__all__ = ["VCSys", "VCGeometry", "VCEvents"]
--- pyvc/pyvc/vcplots.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-
-def magnitude_rupture_area(sim_file, output_file):
-    #Instantiate VCEvents, VCGeometry classes
-    pass
